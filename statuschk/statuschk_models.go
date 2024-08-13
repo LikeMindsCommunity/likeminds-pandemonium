33584107b646022047d2e36b2000b3ef739527ea
Ketan Yadav <ketan@likeminds.community>
2024-08-13 11:56:04 +0530

    Cleanup previous commits

--- statuschk/statuschk_models.go
+++
@@ -1,8 +0,0 @@
-package statuschk
-
-// Response defines statuschkresposne GET API response body
-type Response struct {
-	Status int    `json:"status_code" binding:"required"`
-	Data   string `json:"data"`
-	Error  string `json:"error"`
-}
